2294add2c5f9d1f46bd40d8bfbdf1a382d11294c
jotazet <jakub.zydron@microsoft.wsei.edu.pl>
2025-04-01 23:34:57 +0200

    Create class Konto

---
+++ Bank/Bank/Konto.cs
@@ -0,0 +1,35 @@
+﻿namespace Bank
+{
+    public class Konto
+    {
+        public string Klient { get; }
+        public decimal Bilans { get; private set; }
+        public bool Zablokowane { get; private set; }
+        public Konto(string klient, decimal bilans = 0)
+        {
+            this.Klient = klient;
+            this.Bilans = bilans;
+        }
+
+        void Wplata(decimal kwota)
+        {
+            if (this.Zablokowane)
+                throw new System.Exception("Konto zablokowane");
+            if (kwota <= 0)
+                throw new System.Exception("Kwota musi być dodatnia");
+            this.Bilans += kwota;
+        }
+
+        void Wyplata(decimal kwota)
+        {
+            if (this.Zablokowane)
+                throw new System.Exception("Konto zablokowane");
+            if (kwota <= 0)
+                throw new System.Exception("Kwota musi być dodatnia");
+            if (kwota > this.Bilans)
+                throw new System.Exception("Brak środków na koncie");
+            this.Bilans -= kwota;
+        }
+
+    }
+}
