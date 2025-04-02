f08be82a909cdd0d687a80a3868949ec524d2e7d
jotazet <jakub.zydron@microsoft.wsei.edu.pl>
2025-04-02 23:11:28 +0200

    KontoPlus impleementation

--- Bank/Bank/Konto.cs
+++ Bank/Bank/Konto.cs
@@ -3,14 +3,16 @@
     public class Konto
     {
         public string Klient { get; }
+        private decimal bilans;
         public decimal Bilans { get; private set; }
+
         public bool Zablokowane { get; private set; }
         public Konto(string klient, decimal bilans = 0)
         {
+            if (bilans < 0)
+                throw new ArgumentException("Bilans nie może być ujemny");
             if (string.IsNullOrWhiteSpace(klient))
                 throw new ArgumentException("Nazwa klienta nie może być pusta");
-            if (bilans < 0)
-                throw new ArgumentException("Kwota musi być dodatnia lub równa zero");
             this.Klient = klient;
             this.Bilans = bilans;
         }
@@ -43,6 +45,67 @@
         public void OdblokujKonto()
         {
             this.Zablokowane = false;
+        }
+    }
+
+    public class KontoPlus : Konto
+    {
+        public new decimal Bilans { get; private set; }
+        private bool debetUzyty;
+        private decimal debet;
+        public decimal Debet
+        {
+            get => debet;
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentException("Debet musi być dodatni lub równy zero");
+                debet = value;
+            }
+        }
+        public KontoPlus(string klient, decimal bilans = 0, decimal debet = 0) : base(klient, bilans)
+        {
+            this.Debet = debet;
+        }
+        public void ZwiekszenieLimitu(decimal debet)
+        {
+            if (debet <= 0)
+                throw new ArgumentException("Kwota musi być dodatnia");
+            if (debet > Debet)
+                debetUzyty = false;
+            Debet += debet;
+        }
+
+        public void ZmniejszenieLimitu(decimal kwota)
+        {
+            if (kwota > Debet)
+                throw new ArgumentException("Kwota nie może być większa niż aktualny debet");        
+            Debet -= kwota;
+        }
+
+        public new void Wplata(decimal kwota)
+        {
+            base.Wplata(kwota);
+            if (Bilans > 0)
+            {
+                OdblokujKonto();
+                debetUzyty = false;
+            }
+        }
+        public new void Wyplata(decimal kwota)
+        {
+            if (Zablokowane)
+                throw new ArgumentException("Konto zablokowane");
+            if (kwota <= 0)
+                throw new ArgumentException("Kwota musi być dodatnia");
+            if (kwota > Bilans)
+            {
+                if (debetUzyty || kwota > base.Bilans + Debet)
+                    throw new ArgumentException("Brak środków na koncie");
+                debetUzyty = true;
+                BlokujKonto();
+            }
+            this.Bilans -= kwota;
         }
     }
 }
--- Bank/Test1/Test.cs
+++ Bank/Test1/Test.cs
@@ -2,7 +2,7 @@
 namespace Test
 {
     [TestClass]
-    public sealed class Test
+    public sealed class KontoTest
     {
         [TestMethod]
         public void TworzenieKonta()
@@ -141,6 +141,200 @@ namespace Test
             // Act
             // Assert
             Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(kwota));
+        }
+        [TestClass]
+        public sealed class KontoPlusTest
+        {
+            [TestMethod]
+            public void TworzenieKontaPlus()
+            {
+                // Arrage
+                var klient = "Jan";
+                var bilans = 1000;
+                var debet = 100;
+                // Act
+                var konto = new KontoPlus(klient, bilans, debet);
+                // Assert
+                Assert.AreEqual(klient, konto.Klient);
+                Assert.AreEqual(bilans, konto.Bilans);
+                Assert.AreEqual(debet, konto.Debet);
+            }
+            [TestMethod]
+            public void TworzenieKontaPlusDebetMniejZero()
+            {
+                // Arrage
+                var klient = "Jan";
+                var bilans = 1000;
+                var debet = -100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => new KontoPlus(klient, bilans, debet));
+            }
+            [TestMethod]
+            public void TworzenieKontaPlusDebetZero()
+            {
+                // Arrage
+                var klient = "Jan";
+                var bilans = 1000;
+                var debet = 0;
+                // Act
+                var konto = new KontoPlus(klient, bilans, debet);
+                // Assert
+                Assert.AreEqual(klient, konto.Klient);
+                Assert.AreEqual(bilans, konto.Bilans);
+                Assert.AreEqual(debet, konto.Debet);
+            }
+            [TestMethod]
+            public void TworzenieKontaPlusBrakNazwy()
+            {
+                // Arrage
+                var klient = "";
+                var bilans = 1000;
+                var debet = 100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => new KontoPlus(klient, bilans, debet));
+            }
+            [TestMethod]
+            public void ZwiekszenieLimitu()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var debet = 100;
+                // Act
+                konto.ZwiekszenieLimitu(debet);
+                // Assert
+                Assert.AreEqual(200, konto.Debet);
+            }
+            [TestMethod]
+            public void ZwiekszenieLimituMniejZero()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var debet = -100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.ZwiekszenieLimitu(debet));
+            }
+            [TestMethod]
+            public void ZwiekszenieLimituDebetZero()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var debet = 0;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.ZwiekszenieLimitu(debet));
+            }
+            [TestMethod]
+            public void Wplata()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = 100;
+                // Act
+                konto.Wplata(kwota);
+                // Assert
+                Assert.AreEqual(1100, konto.Bilans);
+            }
+            [TestMethod]
+            public void WplataMniejZero()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = -100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.Wplata(kwota));
+            }
+            [TestMethod]
+            public void WplataKontoZablokowane()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                konto.BlokujKonto();
+                var kwota = 100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.Wplata(kwota));
+            }
+            [TestMethod]
+            public void Wyplata()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = 100;
+                // Act
+                konto.Wyplata(kwota);
+                // Assert
+                Assert.AreEqual(900, konto.Bilans);
+            }
+            [TestMethod]
+            public void WyplataMniejZero()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = -100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(kwota));
+            }
+            [TestMethod]
+            public void WyplataKontoZablokowane()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                konto.BlokujKonto();
+                var kwota = 100;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(kwota));
+            }
+            [TestMethod]
+            public void WyplataDebet()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = 1100;
+                // Act
+                konto.Wyplata(kwota);
+                // Assert
+                Assert.AreEqual(-100, konto.Bilans);
+            }
+            [TestMethod]
+            public void WyplataDebetBrakSrodkow()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = 1200;
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(kwota));
+            }
+            [TestMethod]
+            public void WyplataDebetBrakSrodkowDebet()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = 1100;
+                konto.Wyplata(kwota);
+                // Act
+                // Assert
+                Assert.ThrowsException<ArgumentException>(() => konto.Wyplata(100));
+            }
+            [TestMethod]
+            public void WyplataDebetBrakSrodkowDebetZwiekszenie()
+            {
+                // Arrage
+                var konto = new KontoPlus("Jan", 1000, 100);
+                var kwota = 1100;
+                konto.Wyplata(kwota);
+                // Act
+                konto.ZwiekszenieLimitu(100);
+                konto.Wyplata(100);
+                // Assert
+                Assert.AreEqual(-100, konto.Bilans);
+            }
         }
     }
 }
